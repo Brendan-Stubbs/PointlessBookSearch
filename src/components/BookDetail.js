9e47d745ffadad1a66ce6e98099a3266c9e697c6
Brendan Stubbs <stubbsbrendan@gmail.com>
2020-06-28 09:28:44 +0200

    set default book image

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -4,14 +4,14 @@ const BookDetail = ({ book }) => {
   if (!book) {
     return <div>Loading...</div>;
   }
+
+  const img = book.volumeInfo.imageLinks
+    ? book.volumeInfo.imageLinks.thumbnail
+    : "https://dpcpa.com/wp-content/uploads/2015/01/thumbnail-default.jpg";
   return (
     <div>
       <div>
-        <img
-          className="ui small center image"
-          src={book.volumeInfo.imageLinks.thumbnail}
-          alt=""
-        />
+        <img className="ui small center image" src={img} alt="" />
       </div>
       <h4 className="ui header">{book.volumeInfo.title}</h4>
       <h5 className="ui header">{book.volumeInfo.authors}</h5>
--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -2,11 +2,14 @@ import "./BookItem.css";
 import React from "react";
 
 const BookItem = ({ book, onBookSelect }) => {
+  const img = book.volumeInfo.imageLinks
+    ? book.volumeInfo.imageLinks.smallThumbnail
+    : "https://dpcpa.com/wp-content/uploads/2015/01/thumbnail-default.jpg";
   return (
     <div className="item book-item" onClick={() => onBookSelect(book)}>
       <img
         className="ui centered image small-image"
-        src={book.volumeInfo.imageLinks.smallThumbnail}
+        src={img}
         alt={book.volumeInfo.title}
       />
       <div className="content">
